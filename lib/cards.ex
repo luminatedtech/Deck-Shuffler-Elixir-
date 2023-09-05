d3042b43b437c70ad1b559cf63b4c70932a97559
Allen Kim <avalmercafe02@gmail.com>
2023-09-04 22:35:10 -0400

    refactoring error message

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,27 +3,29 @@ defmodule Cards do
  def create_deck do
   values = ["Ace","Two","Three","Four","Five"]
   suits = ["Spades","Diamonds","Hearts","Clubs"]
-  
+
   for value <- values, suit <- suits  do
     "#{value} of #{suit}"
-  end 
- end 
- 
- def shuffle(deck) do 
+  end
+ end
+
+ def shuffle(deck) do
     Enum.shuffle(deck)
- end 
- def contains?(deck,card) do 
+ end
+ def contains?(deck,card) do
     Enum.member?(deck,card)
- end  
- def deal(deck,hand_size) do 
+ end
+ def deal(deck,hand_size) do
   Enum.split(deck,hand_size)
- end 
- def save(deck,filename) do 
+ end
+ def save(deck,filename) do
   binary = :erlang.term_to_binary(deck)
   File.write(filename,binary)
- end 
- def load(filename) do 
-  {status,binary} = File.read(filename)
-  :erlang.binary_to_term binary
- end 
+ end
+ def load(filename) do
+  case File.read(filename) do
+    {:ok, binary} -> :erlang.binary_to_term binary
+    {:error,_reason} -> "That file does not exist"
+    end
+ end
 end
