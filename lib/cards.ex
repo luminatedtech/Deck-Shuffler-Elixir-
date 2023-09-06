9a9d5e8a83001666e9bf99032e314cd3efd4fbec
Allen Kim <avalmercafe02@gmail.com>
2023-09-05 23:37:10 -0400

    module and function documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,10 @@
 defmodule Cards do
-
+@moduledoc """
+ Provides methods for creating and handling a deck of cards
+"""
+@doc """
+Returns a list of strings representing a deck of playing cards
+"""
  def create_deck do
   values = ["Ace","Two","Three","Four","Five"]
   suits = ["Spades","Diamonds","Hearts","Clubs"]
@@ -15,6 +20,18 @@ defmodule Cards do
  def contains?(deck,card) do
     Enum.member?(deck,card)
  end
+ @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates the number of cards in hand
+
+	## Examples
+
+			iex> deck = Cards.create_deck
+    	iex> {hand,deck} = Cards.deal(deck,1)
+    	iex> hand
+    	["Ace of Spades"]
+
+ """
  def deal(deck,hand_size) do
   Enum.split(deck,hand_size)
  end
@@ -27,5 +44,11 @@ defmodule Cards do
     {:ok, binary} -> :erlang.binary_to_term binary
     {:error,_reason} -> "That file does not exist"
     end
+ end
+ def create_hand(hand_size) do
+
+   Cards.create_deck
+   |> Cards.shuffle
+   |> Cards.deal(hand_size)
  end
 end
